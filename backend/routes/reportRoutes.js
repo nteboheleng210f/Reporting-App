856de44937f3bdaa36b6ed6733b17c6e657b96f2
Ntebo Mphapang <nteboheleng210f>
2026-05-05 14:49:30 +0200

    fixes

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -4,21 +4,11 @@ const {
   createReport, 
   getReports, 
   getMyReports,
-  getReportsPaginated,        
-  getPendingReportsPaginated, 
   updateReportFeedback, 
   getPendingReports, 
   getReviewedReports, 
-  exportReportsToExcel,
-  submitStructuredFeedback    
+  exportReportsToExcel
 } = require('../controllers/reportController');
-const { getUserFromHeader, checkRole } = require('../middleware/auth');
-
-router.use(getUserFromHeader);
-
-
-router.get('/paginated', checkRole(['pl', 'prl']), getReportsPaginated);
-router.get('/pending/paginated', checkRole(['prl']), getPendingReportsPaginated);
 
 
 router.get('/', getReports);
@@ -26,8 +16,7 @@ router.get('/pending', getPendingReports);
 router.get('/reviewed', getReviewedReports);
 router.get('/my', getMyReports);
 router.get('/export', exportReportsToExcel);
-router.post('/', checkRole(['lecturer']), createReport);
-router.put('/:reportId/feedback', checkRole(['prl']), updateReportFeedback);
-router.post('/:reportId/structured-feedback', checkRole(['prl']), submitStructuredFeedback); // NEW
+router.post('/', createReport);
+router.put('/:reportId/feedback', updateReportFeedback);
 
 module.exports = router;
